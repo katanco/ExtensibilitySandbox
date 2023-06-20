c4ab6350c8575fc655028fb41ae81db42fe72cf8
jonathan-schmitt-mx <jonathan.schmitt@mendix.com>
2023-06-20 09:39:05 -0600

    added entity from csv

--- ExtensibilitySandbox/ExtensibilitySandbox.cs
+++ ExtensibilitySandbox/ExtensibilitySandbox.cs
@@ -3,17 +3,20 @@ using Mendix.StudioPro.ExtensionsAPI.UI.Menu;
 using System.ComponentModel.Composition;
 using Mendix.StudioPro.ExtensionsAPI.Model.Projects;
 using Mendix.StudioPro.ExtensionsAPI.Model;
-
+using Mendix.StudioPro.ExtensionsAPI.UI.Services;
 
 namespace Mendix.Extensibility.ExtensibilitySandbox;
 
 [Export(typeof(MenuBarExtension))]
 public class TestExtension : MenuBarExtension
 {
+
+    private readonly IDockingWindowService dockingWindowService;
+
     [ImportingConstructor]
     public TestExtension()
     {
-        // constructor
+        this.dockingWindowService = dockingWindowService;
     }
 
     public override IEnumerable<MenuViewModelBase> GetMenus()
@@ -60,6 +63,53 @@ public class TestExtension : MenuBarExtension
                     }
 
                     MessageBox.Show("goodbye :)");
+                }
+            }
+        };
+
+        yield return new MenuItemViewModel("Entity from csv", placeUnder: new[] { "app" }, placeAfter: "tools")
+        {
+            Action = () =>
+            {
+                if (CurrentApp != null)
+                {
+                    var ofd = new Eto.Forms.OpenFileDialog
+                    {
+                        MultiSelect = false,
+                        Title = "Open"
+                    };
+
+                    var result = ofd.ShowDialog(new Panel());
+
+                    if (result == DialogResult.Ok)
+                    {
+                        using (var reader = new StreamReader(ofd.FileName))
+                        {
+                            while (!reader.EndOfStream)
+                            {
+                                var line = reader.ReadLine();
+                                var attributes = line.Split(',');
+
+                                using ITransaction transaction = CurrentApp.StartTransaction("Entity from sheet");
+                                IProject CurrentProject = (IProject)CurrentApp.Root.Container;
+                                foreach (var module in CurrentProject.GetModules())
+                                {
+                                    if (!module.FromAppStore)
+                                    {
+                                        // TODO: change to selection
+                                        var TargetEntity = module.DomainModel.GetEntities()[0];
+                                        foreach (var attribute in attributes)
+                                        {
+                                            // TODO: add attributes
+                                            MessageBox.Show(attribute.ToString());
+                                        }
+                                        break;
+                                    }
+                                }
+                                transaction.Commit();
+                            }
+                        }
+                    }
                 }
             }
         };
---
+++ ExtensibilitySandbox/TestPane.cs
@@ -0,0 +1,22 @@
+﻿namespace ExtensibilitySandbox;
+
+using Mendix.StudioPro.ExtensionsAPI.UI.DockablePane;
+using System.ComponentModel.Composition;
+using Eto.Forms;
+
+
+[Export(typeof(DockablePaneExtension))]
+class TestPane : DockablePaneExtension
+{
+
+    //public DockablePanel() { }
+
+    public override DockablePaneViewModel Open()
+    {
+
+        return new DockablePaneViewModel { Title = "My Dockable Pane", Controls = { new StackLayoutItem(new Panel(), true) } };
+    }
+
+    public const string ID = "test-pane-1";
+    public override string Id => ID;
+}
